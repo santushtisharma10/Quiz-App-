23cdf4711562a32ab9140e8c5a705af3191b679c
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-04-11 19:35:54 +0530

    changes in design

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,50 +1,52 @@
-//import logo from './logo.svg';
+
 import './App.css';
 import React from "react"
 
 function App() {
   return (
     <div>
-      <h1>Welcome to the quiz app <br />Choose the correct fields and start the quiz</h1>
-      <br /><br />
-      
-        <div class="row padding">
-          
-          <div class="col-md-3">
-        <select >
-          <option selected>Difficulty Level</option>
-          <option value="easy">Easy</option>
-          <option value="med">Medium</option>
-          <option value="hard">Hard</option>
-        </select>
+      <div className="heading">
+        <h1>Welcome to the quiz app <br />Choose the correct fields and start the quiz</h1>
+
+      </div>
+      <br />
+      <div className="row padding">
+
+        <div className="col-md-3">
+          <select >
+            <option selected data-default>Difficulty Level</option>
+            <option value="easy">Easy</option>
+            <option value="med">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
         </div>
-        <div class="col-md-3">
-        <select>
-          <option selected>Category</option>
-          <option value="">General Knowledge</option>
-          <option value="">History</option>
-        </select>
+        <div className="col-md-3">
+          <select>
+            <option value="" selected data-default>Category</option>
+            <option value="">General Knowledge</option>
+            <option value="">History</option>
+          </select>
         </div>
-        <div class="col-md-3">
-        <select>
-          <option selected>Number of Questions</option>
-          <option value="">10</option>
-          <option value="">15</option>
-        </select>
+        <div className="col-md-3">
+          <select>
+            <option selected data-default>Number of Questions</option>
+            <option value="">10</option>
+            <option value="">15</option>
+          </select>
         </div>
-        <div class="col-md-3">
-        <select>
-          <option selected>type</option>
-          <option value="">dont know</option>
-          <option value="">what to write here</option>
-        </select>
+        <div className="col-md-3">
+          <select>
+            <option selected>type</option>
+            <option value="">dont know</option>
+            <option value="">what to write here</option>
+          </select>
         </div>
-       <br /><br />
+        <br /><br />
         <div class="col-12">
-          <button class="btn btn-primary">Submit</button>
-        </div>        
+          <button >Submit</button>
         </div>
-      
+      </div>
+
     </div>
   );
 }

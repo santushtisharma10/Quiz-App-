b743d037f1ed94a27b85142f8b85bedad78b535d
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-07-28 18:50:34 +0530

    changes made in landing page of quiz app

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,15 +1,26 @@
-
 import './App.css';
 import React from "react"
 
 function App() {
   return (
+
     <div className="main">
+      
       <div className="card1">
+        <br />
+        <br />
         <br />
         <br />
         <h1 className="glow">Quizzy</h1>
         <br />
+
+        <h1 className="tagline">A good day to Quiz hard</h1>
+        
+        <br />
+        <br />
+        <br />
+        <br />
+      
         <button >Enter to get started</button>
       </div>
     

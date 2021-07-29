7bdc7bdb8be967d1db9377f2635c1dd4ddde9106
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-07-29 14:25:32 +0530

    changes made

--- quiz/src/components/card.js
+++ quiz/src/components/card.js
@@ -1,15 +1,22 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "./card.css"
 
 
 export default Card
 function Card() {
+  
+    const[enter, setEnter] = useState("visible")
+    
+    useEffect(() => {
+      console.log(enter)
+    }, [])
+
     return (
 
         <div className="main">
           
           <div className="card1">
-          
+          <div className="text" style={{display: enter}}>
               <br />
             <br />
             <br />
@@ -25,7 +32,9 @@ function Card() {
             <br />
             <br />
           
-            <button >Enter to get started</button>
+            <button type="button" onClick={()=> setEnter("none")}>Enter to get started</button>
+            
+        </div>
         </div>
           
         
